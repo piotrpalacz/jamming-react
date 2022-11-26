a3f0373838ade99644723e4f263f00d73a2d2682
KasiaPiciu <peterpalacz12@gmail.com>
2022-11-26 03:02:33 +0100

    Final Project

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import './SearchBar.css'
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    }
+
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search() {
+    this.props.onSearch(this.state.term)
+  }
+
+  handleTermChange(event) {
+    this.setState({ term: event.target.value });
+  }
+  
+  render() {
+    return (
+      <div className='SearchBar'>
+        <input onChange={this.handleTermChange} placeholder='Enter a Song, Album, or Artist' />
+        <button className='SearchButton' onClick={this.search}>SEARCH</button>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
